80fd464ffad3ef13127f034d68c7b5a6536702f4
sam_ <45014739+sbordel@users.noreply.github.com>
2024-12-02 13:22:27 -0500

    fixed mobile version and added sound clips to buttons

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var baseUrl = "assets/audio/";
+    var audio = ["pw-sako.mp3", "pw-simon.mp3", "pw-sam.mp3"];
+
+
+$('.tiles').click(function(){
+    var i = $(this).attr("id").substring(1);  
+    new Audio(baseUrl + audio[i - 1]).play(); 
+});
+
+
+});
